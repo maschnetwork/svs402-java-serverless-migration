becf2d8d86aca9510f14bbd8ea771a328675e126
Mark Sailes <msailes@amazon.co.uk>
2023-10-03 16:26:49 +0100

    start of the rest api

---
+++ after/rest-api/src/main/java/software/serverlessflix/claps/ClapsController.java
@@ -0,0 +1,23 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: MIT-0
+
+package software.serverlessflix.claps;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/claps")
+public class ClapsController {
+
+    private final ClapsService clapsService;
+
+    public ClapsController(ClapsService clapsService) {
+        this.clapsService = clapsService;
+    }
+
+    public ClapsResponse claps(String videoId) {
+        int claps = clapsService.getClaps(videoId);
+    }
+
+}
